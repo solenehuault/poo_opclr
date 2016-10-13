87734a775c8f57c52cbba2b4106b14d145e7c90e
solenehuault <mopi@dotslashplay.it>
2016-10-13 14:13:51 +0200

    Personnage.php - ajout des méthodes getters et suppression de la méthode afficher_experience devenue obsolète

--- Personnage.php
+++ Personnage.php
@@ -5,14 +5,31 @@ class Personnage {
 	private $_experience = 0;
 	private $_degats = 5;
 
+	//accesseur (getters)
+	public function get_force() {
+		return $this->_force;
+	}
+
+	public function get_localisation() {
+		return $this->_localisation;
+	}
+
+	public function get_experience() {
+		return $this->_experience;
+	}
+
+	public function get_degats() {
+		return $this->_degats;
+	}
+
+	//methods
 	public function deplacer() {}
+
 	public function frapper(Personnage $perso_a_frapper) {
 		//le perso_a_frapper va voir ses dégâts augmenter en fonction de la force du perso qui le frappe
 		$perso_a_frapper->_degats += $this->_force;
 	}
-	public function afficher_experience() {
-		echo $this->_experience;
-	}
+
 	public function gagner_experience() {
 		$this->_experience++;
 	}
