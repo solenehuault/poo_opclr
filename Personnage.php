9024d2566d9894c9ca1f8f20c1c2fc2c7d4a445a
solenehuault <mopi@dotslashplay.it>
2016-10-13 12:26:42 +0200

    initial commit

---
+++ Personnage.php
@@ -0,0 +1,11 @@
+<?php
+class Personnage {
+	private $_force;
+	private $_localisation;
+	private $_experience;
+	private $_degats;
+
+	public function deplacer() {}
+	public function frapper() {}
+	public function gagnerexperience() {}
+}
