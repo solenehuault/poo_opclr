fb22437fcc646579925c99275f6c402df51960a9
solenehuault <mopi@dotslashplay.it>
2016-10-13 14:04:46 +0200

    index.php - suite de méthode permettant de frapper un perso puis l’autre

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 
 require 'Personnage.php';
 
-$perso = new Personnage;
-$perso->afficher_experience();
-$perso->gagner_experience();
-$perso->afficher_experience();
+$truc = new Personnage;
+$mopi = new Personnage;
 
+$truc->frapper($mopi);
+$truc->gagner_experience();
+
+$mopi->frapper($truc);
+$mopi->gagner_experience();
+
+$truc->afficher_experience();
+$mopi->afficher_experience();
 ?>
