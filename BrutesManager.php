232576a08ec5ac4e1e3048445f540b2110aa1fb8
solenehuault <mopi@dotslashplay.it>
2016-10-14 12:19:32 +0200

    BrutesManager.php - class created with associated methods

---
+++ BrutesManager.php
@@ -0,0 +1,40 @@
+<?php
+class BrutesManager {
+	private $_bd;
+
+	public function __construct($db) {
+		$this->set_db($db);
+	}
+
+	public function set_db($bd) {
+		$this->_db = $db;
+	}
+
+	public function exists($info) {
+
+	}
+
+	public function get($info) {
+
+	}
+
+	public function add(Brute $brute) {
+	}
+
+	public function update(Brute $brute) {
+		
+	}
+
+	public function delete(Brute $brute) {
+
+	}
+
+	public function count() {
+
+	}
+
+	public function get_list($name) {
+
+	}
+}
+?>
