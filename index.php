46d4b41d33c05ae8742d6c4ca87e02931814e9f5
solenehuault <mopi@dotslashplay.it>
2016-10-14 14:34:03 +0200

    index.php - first display

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Brute Game</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<h1>Brute Game</h1>
+		<form method="post" action="">
+			<label for="name">Name:</label>
+			<input id="name" name="name" type="text" />
+			<button form="form" name="create" type="submit">Create this Brute</button>
+			<button form="form" name="use" type="submit">Use this Brute</button>
+		</form>
+	</body>
+</html>
